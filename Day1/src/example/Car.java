4bf69b3cf875a54a662d29fe645a749bafc95a3e
Naveen Hosavaranchi Nagendra <nhosavaranch@BLR.ALLEGISINDIA.COM>
2023-02-24 09:41:36 +0530

    Day2 Learning

---
+++ Day1/src/example/Car.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Car {
+	
+	String colour;
+	int noofwheels;
+	
+	public static String maunfacturer="Honda";
+
+	public Car() {
+		colour="green";
+		noofwheels=4;
+	}
+	
+}
--- Day1/src/example/Demo2.java
+++ Day1/src/example/Demo2.java
@@ -1,6 +1,10 @@
 package example;
 
 public class Demo2 {
+	
+	static {
+		System.out.println("From static method");
+	}
 
 	public Demo2() {
 		System.out.println("From constructor");
@@ -12,6 +16,8 @@ public class Demo2 {
 		System.out.println("From main method");
 		
 		Demo2 d=new Demo2();
-		
+		Car c=new Car();
+		System.out.println(c.colour);
+		System.out.println(Car.maunfacturer);
 	}
 }
